05eea6e5f857f06dd29aae78b621a011a440e979
skMontajulAli <sekhmontajulali1997@gmail.com>
2024-07-31 09:24:41 +0530

    backent to fontend connect

--- src/apiAxiosInstance/api.js
+++ src/apiAxiosInstance/api.js
@@ -6,12 +6,14 @@ const foget = "";
 console.log(Cookie.get("forgetPasswordToken"));
 
 export const API = axios.create({
-  baseURL: "http://localhost:5050/api/v1",
+  baseURL:
+    "https://authonticationauthoriation-backend-latest.onrender.com/api/v1",
   withCredentials: true,
 });
 
 export const APIForVerify = axios.create({
-  baseURL: "http://localhost:5050/api/v1",
+  baseURL:
+    "https://authonticationauthoriation-backend-latest.onrender.com/api/v1",
   headers: { fogetPass: `Bearer ${foget}` },
   withCredentials: true,
 });
