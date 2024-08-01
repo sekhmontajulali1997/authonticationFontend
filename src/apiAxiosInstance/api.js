4d341edb4b2637e5dc3212580cc142626cbbca4f
skMontajulAli <sekhmontajulali1997@gmail.com>
2024-08-01 09:30:34 +0530

    again

--- src/apiAxiosInstance/api.js
+++ src/apiAxiosInstance/api.js
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-
-
-
 export const API = axios.create({
   baseURL:
     "https://authonticationauthoriation-backend-latest.onrender.com/api/v1",
   withCredentials: true,
 });
-
-
